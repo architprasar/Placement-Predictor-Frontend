7abb1e4994b7053365d23234c4659a37a04d4fc8
Archit Prasar <architprasar@gmail.com>
2023-06-18 13:48:15 +0530

    front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import MyComponent from './aiect.js'
+import Cocubes from './cocubes';
 function App() {
+  const [model, setModel] = useState(false);
+
+  useEffect(() => { }, [model])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {model === true ? <Cocubes setModel={setModel} /> : <MyComponent setModel={setModel} />}
     </div>
   );
 }
